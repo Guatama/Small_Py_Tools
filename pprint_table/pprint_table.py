221b50688fc70cce1b1760446db2d9450550558c
Guatama <flatline.mail@gmail.com>
2020-02-20 12:27:59 +0500

    Function for printing table in terminal

--- pprint_table/pprint_table.py
+++ pprint_table/pprint_table.py
@@ -1,52 +1,78 @@
 import typing as tp
+from warnings import warn
+from platform import python_version_tuple
+
 
 def pprint_table(list_with_data: tp.List[tp.Dict[str, str]],
                  main_attr: tp.List[str]=None,
-                 limit: int=None, 
+                 header: tp.List[str]=None,
+                 limit: int=None,
                  full: bool=False) -> None:
-    """[summary]
-    
-    Arguments:
-        list_with_data {[List[Dict[str, str]], ...]} -- [description]
-    
-    Keyword Arguments:
-        main_attr {[List[str]]} -- [description] (default: {None})
-        limit {int} -- [description] (default: {None})
-        full {bool} -- [description] (default: {False})
-    """
-    attr_list = list(list_with_data[0].keys())
+    """Printing tables in the terminal
 
-    attr_config = dict.fromkeys(attr_list, False)
+    Arguments:
+        list_with_data->List[Dict[str, str]], ...] -- input data in format:
+        [ One elemnt in list - one line of table
+            {
+            'Column_name': 'Value_in_line',
+            'Column_name': 'Value_in_line',
+            ...
+            },
+            ...
+        ]
+
+    Keyword Arguments:
+        main_attr->List[str] -- List of columns to display (default: None)
+        limit->int -- Limit the number of rows in a table (default: None)
+        full->bool -- Show all columns from data (default: bool->False)
+    """
+    # Check that user has Python 3.7+
+    py_version = python_version_tuple()
+    is_py3 = int(py_version[0]) >= 3
+    is_py37 = is_py3 and int(py_version[1]) >= 7
+    if not is_py3 or not is_py37:
+        warn('Your version is, the order of columns in the table')
+
+    # Creating basic configurations for displaying a table
+    attr_list: tp.List[str] = list(list_with_data[0].keys())
+    attr_config: tp.Dict[str, bool] = dict.fromkeys(attr_list, False)
+
+    main_attr = attr_list if main_attr is None else main_attr
+    header = attr_list if header is None else header
+    header_config: tp.Dict[str, str] = {
+        attr_key: header_name for (attr_key, header_name)
+        in zip(attr_list, header)
+    }
+
+    # Determining how many columns should be displayed
     if main_attr is None:
         full = True
     else:
         for item in main_attr:
             attr_config[item] = True
 
-    len_dict = {}
+    # Creating default dict of columns length
+    len_dict: tp.Dict[str, int] = {}
     for item in attr_list:
         len_dict[item] = len(item)
 
-    # Enter the maximum width for each column in the counter
+    # Enter the maximum width for each column in the data
     for line in list_with_data:
         for item in attr_list:
-            # BUG! all_attr-type of task have separate sets of attributes
-            try:
-                if len_dict[item] < len(str(line[item])):
-                    len_dict[item] = len(str(line[item]))
-            except Exception:
-                continue
+            if len_dict[item] < len(line[item]):
+                len_dict[item] = len(line[item])
 
     # Print header table with delimiter =
-    for item in attr_config:
+    for item in attr_list:
         if attr_config[item] or full:
-            print(f'{item:{len_dict[item]}} : ', end='')
+            print(f'{header_config[item]:{len_dict[item]}} : ', end='')
     print()
-    for item in attr_config:
+    for item in attr_list:
         if attr_config[item] or full:
             print(f'{"=":=<{len_dict[item]}} : ', end='')
     print()
 
+    # Print line of table
     line_counter = 0
     for line in list_with_data:
         # Check limit of output
@@ -56,45 +82,7 @@ def pprint_table(list_with_data: tp.List[tp.Dict[str, str]],
             line_counter += 1
 
         # Print table row
-        for item in attr_config:
+        for item in attr_list:
             if attr_config[item] or full:
                 print(f'{line[item]:{len_dict[item]}} : ', end='')
         print()
-
-if __name__ == "__main__":
-    mock_data = [
-        {
-            'name_of_item': 'folder_1',
-            'fullpath': '//home/folder_2/new_folder/folder_1',
-            'time_when_folder_created': '2020.02.15 15:44:02',
-            'lock_state': ' ',
-            'total_jobs': '10543',
-            'completed_jobs': '154'
-        },
-        {
-            'name_of_item': 'folder_2',
-            'fullpath': '//home/folder_2/new_folder/folder_1',
-            'time_when_folder_created': '2022.01.18 19:42:52',
-            'lock_state': '🔒',
-            'total_jobs': '1543',
-            'completed_jobs': '10'
-        },
-        {
-            'name_of_item': 'folder_3',
-            'fullpath': '//home/folder_2/new_folder/folder_1',
-            'time_when_folder_created': '2009.03.15 15:44:02',
-            'lock_state': '🔒',
-            'total_jobs': '634',
-            'completed_jobs': '45'
-        },
-        {
-            'name_of_item': 'folder_4',
-            'fullpath': '//home/folder_2/new_folder/folder_1',
-            'time_when_folder_created': '2010.01.25 13:44:02',
-            'lock_state': ' ',
-            'total_jobs': '332243',
-            'completed_jobs': '1454'
-        }
-    ]
-
-    pprint_table(mock_data) 
